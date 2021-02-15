babf094f9c1d647394dbfd2483d1e8390bd2c38e
Diego Osvaldo <g11ada18@hackademy.mx>
2021-02-15 19:29:33 -0400

    Estado de componentes

--- demo-react/src/App.js
+++ demo-react/src/App.js
@@ -1,7 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 
-import Person from './components/Person'
+import Person from './components/Person';
+import Counter from './components/Counter';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <Person nombre="Diego" apellido="Osvaldo" edad={36} />
+        <Counter initialValue={1} />
       </header>
     </div>
   );
