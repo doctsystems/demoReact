94cf3675b544aa9e88aba61d0576be4163fca7c3
Diego Osvaldo <g11ada18@hackademy.mx>
2021-02-25 15:06:42 -0400

    Menu hamburguesa

---
+++ menu-hamburguesa/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/Nav/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      App
+    </div>
+  );
+}
+
+export default App;
---
+++ menu-hamburguesa/src/components/Nav/Navbar.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import styled from 'styled-components';
+import Burguer from './Burguer';
+
+const Nav = styled.nav`
+  width: 100%;
+  height: 55px;
+  border-bottom: 2px solid #f1f1f1;
+  padding: 0 20px;
+  display: flex;
+  justify-content: space-between;
+  .logo {
+    padding: 15px 0;
+  }
+`
+
+const Navbar = () => {
+  return (
+    <Nav>
+      <div className="logo">
+        Nav Bar
+      </div>
+      <Burguer />
+    </Nav>
+  )
+}
+
+export default Navbar
