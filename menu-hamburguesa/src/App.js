10abcb783de3351e1b1516727459c991b64ba9eb
Diego Osvaldo <g11ada18@hackademy.mx>
2021-02-28 23:26:43 -0400

    up

--- menu-hamburguesa/src/App.js
+++ menu-hamburguesa/src/App.js
@@ -7,7 +7,7 @@ import Sidebar from './components/Sidebar';
 function App() {
   return (
     <div className="App" id="outer-container">
-      <Sidebar pageWrapId={"page-wrap"} outerContainerId={"App"} />
+      <Navbar pageWrapId={"page-wrap"} outerContainerId={"App"} />
 
       <div id="page-wrap">
         <h1>Click to show menu</h1>
